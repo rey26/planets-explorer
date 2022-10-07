16819310452f78961cceb54277cee97ba4c537db
Matej Malicky <matej.malicky@gmail.com>
2022-10-07 10:10:10 +0200

    feat: add planet and related model

---
+++ database/migrations/2022_10_07_075852_create_planets_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\GravityStandard;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('planets', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('diameter')->index();
+            $table->string('rotation_period')->index();
+            $table->foreignIdFor(GravityStandard::class);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('planets');
+    }
+};
