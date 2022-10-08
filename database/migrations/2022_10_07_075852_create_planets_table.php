ee83dbd35cbc9eb912cdee953b3463ba1ddfdfb4
Matej Malicky <matej.malicky@gmail.com>
2022-10-08 07:43:46 +0200

    feat: index name column on planets table

--- database/migrations/2022_10_07_075852_create_planets_table.php
+++ database/migrations/2022_10_07_075852_create_planets_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
     {
         Schema::create('planets', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->index();
             $table->float('diameter')->index();
             $table->float('rotation_period')->index();
             $table->foreignIdFor(GravityStandard::class);
