169d6f28b7569d3e4dc5b935f6e74c7a3bcd8f75
Matej Malicky <matej.malicky@gmail.com>
2022-10-17 08:49:48 +0200

    feat: test all planet related entities

--- tests/Unit/PlanetTest.php
+++ tests/Unit/PlanetTest.php
@@ -4,7 +4,10 @@ namespace Tests\Unit;
 
 use App\Models\GravityStandard;
 use App\Models\Planet;
+use App\Models\Resident;
+use App\Models\Species;
 use App\Models\Terrain;
+use Database\Factories\SpeciesFactory;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
@@ -23,9 +26,11 @@ class PlanetTest extends TestCase
         $planet = Planet::factory()
             ->for($gravityStandard)
             ->has(Terrain::factory()->count(3))
+            ->has(Resident::factory()->count(10)->has(Species::factory()->count(2)))
             ->create();
 
         $this->assertEquals($gravityStandard->id, $planet->gravityStandard->id);
+        $this->assertEquals($planet->residents->count(), 10);
         $this->assertEquals(3, $planet->terrains->count());
     }
 }
